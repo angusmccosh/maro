2f771f23f66f695bb2c3cf66d10bd58cd13137ea
angusmccosh <angootma@gmail.com>
2019-07-02 09:26:35 -0700

    create

---
+++ android/src/com/angus/mario/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.angus.mario;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.angus.mario.mario;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new mario(), config);
+	}
+}
---
+++ desktop/src/com/angus/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.angus.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.angus.mario.mario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new mario(), config);
+	}
+}
